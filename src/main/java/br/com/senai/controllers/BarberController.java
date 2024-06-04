randsantana/senai_coffee_Mysql_Spring

att backend 17/05/24

src/main/java/br/com/senai/controllers/BarberController.java
@@ -1,0 +1,51 @@
+package br.com.senai.controllers;
+
+import br.com.senai.models.Barber;
+import br.com.senai.repositories.BarberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/barber")
+public class BarberController {
+    @Autowired
+    BarberRepository barberRepository;
+
+    @GetMapping(value = "/all",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Barber> getAllBarber(){
+        return barberRepository.findAll();
+    }
+
+    @PostMapping(value="/createBarber",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Barber createBarber(@RequestBody Barber barber){
+        Barber newBarber = new Barber();
+        newBarber.setName(barber.getName());
+        newBarber.setImg(barber.getImg());
+        return barberRepository.save(newBarber);
+    }
+
+    @PutMapping(value="/updateBarber",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Barber updateBarber(@RequestBody Barber barber){
+        Barber getBarber = barberRepository
+                .findById(barber.getId()).orElseThrow();
+        Barber updateBarber = new Barber();
+        updateBarber.setName(barber.getName());
+        updateBarber.setImg(barber.getImg());
+        return barberRepository.save(updateBarber);
+    }
+
+    @DeleteMapping(value="/deleteBarber/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Barber deleteBarber(@PathVariable Long id){
+        Barber getBarber = barberRepository.findById(id).orElseThrow();
+        barberRepository.delete(getBarber);
+        return getBarber;
+    }
+}

src/main/java/br/com/senai/models/Barber.java
@@ -5,14 +5,12 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serializable;
-
 @Getter
 @Setter
 @Entity
-@Table(name="coffee")
+@Table(name="barber")
 @EqualsAndHashCode(of="id")
-public class Coffee implements Serializable {
+public class Barber {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,7 +19,5 @@ public class Coffee implements Serializable {
     private String name;
 
     @Column(nullable = false)
-    private Double price;
-
-
+    private String img;
 }

randsantana/projeto_final_back

src/main/java/br/com/senai/controllers/BarberController.java
@@ -1,0 +1,52 @@
+package br.com.senai.controllers;
+
+import br.com.senai.models.Barber;
+import br.com.senai.repositories.BarberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/barber")
+public class BarberController {
+    @Autowired
+    BarberRepository barberRepository;
+
+    @GetMapping(value = "/all",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Barber> getAllBarber(){
+        return barberRepository.findAll();
+    }
+
+    @PostMapping(value="/createBarber",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Barber createBarber(@RequestBody Barber barber){
+        Barber newBarber = new Barber();
+        newBarber.setName(barber.getName());
+        newBarber.setDescrption(barber.getDescrption());
+        return barberRepository.save(newBarber);
+    }
+
+    @PutMapping(value="/updateBarber",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Barber updateBarber(@RequestBody Barber barber){
+        Barber getBarber = barberRepository
+                .findById(barber.getId()).orElseThrow();
+        Barber updateBarber = new Barber();
+        updateBarber.setName(barber.getName());
+        updateBarber.setDescrption(barber.getDescrption());
+        return barberRepository.save(updateBarber);
+    }
+
+    @DeleteMapping(value="/deleteBarber/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Barber deleteBarber(@PathVariable Long id){
+        Barber getBarber = barberRepository.findById(id).orElseThrow();
+        barberRepository.delete(getBarber);
+        return getBarber;
+    }
+}

src/main/java/br/com/senai/models/Barber.java
@@ -5,14 +5,12 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serializable;
-
 @Getter
 @Setter
 @Entity
-@Table(name="coffee")
+@Table(name="barber")
 @EqualsAndHashCode(of="id")
-public class Coffee implements Serializable {
+public class Barber {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,7 +19,7 @@ public class Coffee implements Serializable {
     private String name;
 
     @Column(nullable = false)
-    private Double price;
+    private String descrption;
 
 
 }